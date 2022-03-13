7215e2ae43e93b3ffafa0183cba75118a6d529c5
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-13 21:51:13 +0530

    kuch bhi

--- LinkedList2.py
+++ LinkedList2.py
@@ -1,4 +1,5 @@
 from ll import Node
+from ll import LinkedList
 def middlNode( node):
     fast = node
     slow = node
@@ -8,3 +9,27 @@ def middlNode( node):
     if fast.next is not None:
         return slow.next
     return slow
+
+def mergeTwoSortedLists( node1, node2):
+    dummyNode = Node(-1)
+    head = Node()
+    dummyNode.next = head
+    if node1.data <= node2.data:
+        head.data = node1.data
+        node1.head = node1.next
+    else:
+        head.data = node2.data
+        node2.head = node1.next
+    while node1.head is not None or node2 is not None:
+        if node1.data <= node2.data:
+            head.next = Node( node1.data)
+            node1 = node1.next
+        else:
+            head.next = Node( node2.data)
+            node2 = node2.next
+        head = head.next
+    if node1 is None:
+        head.next = node2
+    else:
+        head.next = node1
+    return dummyNode.next
